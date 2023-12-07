306530bfcc21d0a3d9bdc27abde0e9954854ef90
Pal Marci <20556689+palmarci@users.noreply.github.com>
2023-12-07 20:32:11 +0100

    not jarsigner, but apksigner

--- MinimedPatch/rebuild_reinstall.sh
+++ MinimedPatch/rebuild_reinstall.sh
@@ -1,4 +1,4 @@
-jarSigner="../uber-apk-signer.jar"
+apkSigner="/home/marci/Downloads/uber-apk-signer-1.2.1.jar"
 
 if [[ ! -f AndroidManifest.xml ]] ; then
     echo 'you are not in a project folder'
@@ -6,13 +6,13 @@ if [[ ! -f AndroidManifest.xml ]] ; then
 fi
 
 
-if ! test -f "$jarSigner"; then
+if ! test -f "$apkSigner"; then
   echo "Invalid path for the apk signer jar file. Edit this script!\n"
   exit 1
 fi
 
 mkdir -p dist
 touch dist/asd.apk
-rm -rf dist/*.apk && apktool b . && java -jar '$jarSigner' -apks dist/ && adb install dist/*Signed*.apk
+rm -rf dist/*.apk && apktool b . && java -jar "$apkSigner" -apks dist/ && adb install dist/*Signed*.apk
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 /bin/bash "$SCRIPT_DIR/stop_and_clean.sh"
